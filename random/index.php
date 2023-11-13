963e99243301bd50b7013cdfd5c46b60209347c3
TophRelimbo <133744872+TophRelimbo@users.noreply.github.com>
2023-11-13 18:08:53 +0800

    Create index.php

---
+++ random/index.php
@@ -0,0 +1,17 @@
+<?php
+class Student{
+public function getstudentList($yearlevel = null){
+}
+ public function create() {
+ }
+  public function retrieve() {
+ }
+  public function update() {
+ }
+  public function delete() {
+ }
+}
+class Course{
+}
+
+?>
